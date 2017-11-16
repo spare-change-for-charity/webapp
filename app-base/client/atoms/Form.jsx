2c61883dc1b7e1bfe0975773a64665dd99411bd7
Merlin Patterson <merlinpatt@gmail.com>
2017-11-16 07:16:14 -0500

    Improve form parsing for checkboxes and radios

--- app-base/client/atoms/Form.jsx
+++ app-base/client/atoms/Form.jsx
@@ -6,9 +6,27 @@ class Form extends React.Component {
   onSubmitWrapper = (event) => {
     event.preventDefault();
     const form = event.target;
-    const formData = Array.from(form.elements).reduce((object, input) => (
-      input.name ? {...object, [input.name]: input.value} : object
-    ), {});
+
+    const formData =
+      Array.from(form.elements)
+      .filter(element => element.tagName.toLowerCase() === 'input')
+      .reduce((data, input) => {
+        const {checked, name, type} = input;
+        const value = type === 'checkbox' || type === 'radio' ? checked && input.value : input.value;
+
+        if (! value) {
+          return data[name] ? data : {...data, [name]: value};
+        }
+
+        const oldValue = data[name];
+        if (type === 'checkbox' && oldValue) {
+          const arrayedValue = oldValue.constructor === String ? [oldValue, value] : [...oldValue, value];
+          return {...data, [name]: arrayedValue};
+        }
+
+        return {...data, [name]: value};
+      }, {});
+
     this.props.onSubmit(formData);
   }
 
--- app-base/tests/Form.test.jsx
+++ app-base/tests/Form.test.jsx
@@ -2,13 +2,104 @@ import React from 'react';
 
 import Form from '/client/atoms/Form';
 
-test('Form onSubmitWrapper', () => {
+test('onSubmitWrapper, 1 value', () => {
   const getData = jest.fn();
   const form = mount(
     <Form onSubmit={getData}>
-      <input name="name" defaultValue="value" />
+      <input name="text" defaultValue="value" type="text" />
     </Form>
   );
-  form.find('form').simulate('submit', {preventDefault: jest.fn()});
-  expect(getData).toBeCalledWith({name: 'value'});
+  form.find('form').simulate('submit');
+  expect(getData).toBeCalledWith({text: 'value'});
+});
+
+test('onSubmitWrapper, checkboxes with none selected', () => {
+  const getData = jest.fn();
+  const form = mount(
+    <Form onSubmit={getData}>
+      <input name="checkGroup" value="check1" type="checkbox" />
+      <input name="checkGroup" value="check2" type="checkbox" />
+      <input name="checkGroup" value="check3" type="checkbox" />
+    </Form>
+  );
+  form.find('form').simulate('submit');
+  expect(getData).toBeCalledWith({checkGroup: false});
+});
+
+test('onSubmitWrapper, checkboxes with 1st selected', () => {
+  const getData = jest.fn();
+  const form = mount(
+    <Form onSubmit={getData}>
+      <input name="checkGroup" value="check1" type="checkbox" defaultChecked={true} />
+      <input name="checkGroup" value="check2" type="checkbox" />
+      <input name="checkGroup" value="check3" type="checkbox" />
+    </Form>
+  );
+  form.find('form').simulate('submit');
+  expect(getData).toBeCalledWith({checkGroup: 'check1'});
+});
+
+test('onSubmitWrapper, checkboxes with 2nd selected', () => {
+  const getData = jest.fn();
+  const form = mount(
+    <Form onSubmit={getData}>
+      <input name="checkGroup" value="check1" type="checkbox" />
+      <input name="checkGroup" value="check2" type="checkbox" defaultChecked={true} />
+      <input name="checkGroup" value="check3" type="checkbox" />
+    </Form>
+  );
+  form.find('form').simulate('submit');
+  expect(getData).toBeCalledWith({checkGroup: 'check2'});
+});
+
+test('onSubmitWrapper, checkboxes with 1st and 2nd selected', () => {
+  const getData = jest.fn();
+  const form = mount(
+    <Form onSubmit={getData}>
+      <input name="checkGroup" value="check1" type="checkbox" defaultChecked={true} />
+      <input name="checkGroup" value="check2" type="checkbox" defaultChecked={true} />
+      <input name="checkGroup" value="check3" type="checkbox" />
+    </Form>
+  );
+  form.find('form').simulate('submit');
+  expect(getData).toBeCalledWith({checkGroup: ['check1', 'check2']});
+});
+
+test('onSubmitWrapper, radio group with none selected', () => {
+  const getData = jest.fn();
+  const form = mount(
+    <Form onSubmit={getData}>
+      <input name="radioGroup" value="radio1" type="radio" />
+      <input name="radioGroup" value="radio2" type="radio" />
+      <input name="radioGroup" value="radio3" type="radio" />
+    </Form>
+  );
+  form.find('form').simulate('submit');
+  expect(getData).toBeCalledWith({radioGroup: false});
+});
+
+test('onSubmitWrapper, radio group with 1st selected', () => {
+  const getData = jest.fn();
+  const form = mount(
+    <Form onSubmit={getData}>
+      <input name="radioGroup" value="radio1" type="radio" defaultChecked={true} />
+      <input name="radioGroup" value="radio2" type="radio" />
+      <input name="radioGroup" value="radio3" type="radio" />
+    </Form>
+  );
+  form.find('form').simulate('submit');
+  expect(getData).toBeCalledWith({radioGroup: 'radio1'});
+});
+
+test('onSubmitWrapper, radio group with 2nd selected', () => {
+  const getData = jest.fn();
+  const form = mount(
+    <Form onSubmit={getData}>
+      <input name="radioGroup" value="radio1" type="radio" />
+      <input name="radioGroup" value="radio2" type="radio" defaultChecked={true} />
+      <input name="radioGroup" value="radio3" type="radio" />
+    </Form>
+  );
+  form.find('form').simulate('submit');
+  expect(getData).toBeCalledWith({radioGroup: 'radio2'});
 });
