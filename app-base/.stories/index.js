90f54f338a329b0da588c955da924a9916b9a6ae
Merlin Patterson <merlinpatt@gmail.com>
2017-12-07 16:16:55 -0500

    Remove unreliable require.context

--- app-base/.stories/index.js
+++ app-base/.stories/index.js
@@ -1,11 +1,3 @@
-import React from 'react';
-
-import {storiesOf} from '@storybook/react';
-import {action} from '@storybook/addon-actions';
-import {linkTo} from '@storybook/addon-links';
-
 import '/client/styles/appwide.css';
 
-const requireAll = (requireContext) => requireContext.keys().map(requireContext);
-
-requireAll(require.context('../.stories/atoms', true, /\.jsx$/));
+require('./atoms');
