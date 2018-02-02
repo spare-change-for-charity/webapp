642bcc780301fe002a6919ccbd25d1529883abda
Merlin Patterson <merlinpatt@gmail.com>
2018-02-01 23:46:55 -0500

    Update storyshots

--- app-base/.stories/storyshots.test.js
+++ app-base/.stories/storyshots.test.js
@@ -1,7 +1,12 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import {StyleSheetTestUtils} from 'aphrodite';
 
 jest.mock('../client/lib/randomId', () =>
   string => `${string}-random`
 );
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
 
 initStoryshots();
